238c48024656ac573d0c041fce4619b1838e5187
Prakash Pun <prakash.pun@gmail.com>
2019-05-02 14:56:28 +0530

    Passing data with Event and Property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'What a test', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_960_720.jpg'),
-    new Recipe('Test Recipe', 'What a test', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_960_720.jpg')
+    new Recipe('Test Recipe1', 'What a test1', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Artichoke_Stuffed_Tomato_%285052814731%29.jpg/800px-Artichoke_Stuffed_Tomato_%285052814731%29.jpg'),
+    new Recipe('Test Recipe2', 'What a test2', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Omurice_by_jetalone_in_a_downtown%2C_Tokyo.jpg/800px-Omurice_by_jetalone_in_a_downtown%2C_Tokyo.jpg')
   ];
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
